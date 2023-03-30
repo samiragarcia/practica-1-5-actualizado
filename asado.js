0778781d250ba4fbf46f67a57bd606061b082442
samira garcia <samira.garcia95@gmail.com>
2023-03-30 19:16:54 -0300

    actualizado

---
+++ asado.js
@@ -0,0 +1,17 @@
+document.querySelector("body").innerHTML = `
+<h1>instructivo para hacer un asado</h1>
+
+<ol>
+    <li>buscar el vacio</li>
+    <li>condimentar el vacio con 10 gramos de sal</li>
+    <li>buscar diario, encendedor</li>
+    <li>encender el diario junto al carbon</li>
+    <li>esperar 10 minutos</li>
+    <li>colocar el vacio en la parrilla</li>
+    <li>esperar 40 minutos para dar vuelta el vacio</li>
+    <li>esperar otros 40 minutos</li>
+    <li>sacar el vacio</li>
+</ol>
+
+<img src="https://upload.wikimedia.org/wikipedia/commons/6/65/Asado_2005.jpg" alt="asado">
+    `
